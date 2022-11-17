e7ea64239d0585687877332615ae05c30fc3cc69
Kioolz <caio.vorb@yahoo.com.br>
2022-11-17 09:06:08 -0300

    Create class.py

---
+++ 10-2022/63/class.py
@@ -0,0 +1,17 @@
+class Person: #Do humano
+  def __init__(self, name, age, height):
+    self.name = name
+    self.age = age
+    self.height = height
+
+p1 = Person("John", 36, 170)
+
+p2 = Person("Joao", 10, 120)
+
+
+print(p1.name)
+print(p1.age)
+print(p1.height)
+
+
+
