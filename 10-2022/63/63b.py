96af26e1335927bd6c179fc28f5d7236b8c1febf
Kioolz <caio.vorb@yahoo.com.br>
2022-11-17 09:06:07 -0300

    Create 63b.py

---
+++ 10-2022/63/63b.py
@@ -0,0 +1,8 @@
+#Obs.: Resolva este problema utilizando busca em vetor com intervalos ao invés de
+#encadear estruturas de seleção.
+#Leia um número inteiro que representa um código de DDD para discagem
+#interurbana. Em seguida, informe à qual cidade o DDD pertence, considerando a tabela
+#abaixo
+
+n = int(input())
+
