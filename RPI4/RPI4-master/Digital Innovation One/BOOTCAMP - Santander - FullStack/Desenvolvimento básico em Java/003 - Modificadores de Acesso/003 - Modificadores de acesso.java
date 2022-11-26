6f0c0a968a355d2e181e1acefaee539d1b690098
Kioolz <caio.vorb@yahoo.com.br>
2022-11-25 21:38:51 -0300

    Create 003 - Modificadores de acesso.java

---
+++ RPI4/RPI4-master/Digital Innovation One/BOOTCAMP - Santander - FullStack/Desenvolvimento básico em Java/003 - Modificadores de Acesso/003 - Modificadores de acesso.java
@@ -0,0 +1,20 @@
+// public
+// public pode ser acessada de qualquer lugar por qualquer entidade
+// que possa visualizar a classe a que ela pertence
+
+//Private
+//Os métodos e atributos da classe definido scomo privados
+// não podem ser acessados ou usados por nenhuma outra classe.
+// Esses atributos e métodos também ão podem ser visualizados
+// pelas classes herdadas.
+
+//Protected
+//Torna o membro acessível as classes do mesmo pacote
+// ou através de herança, seus membros herdados não são acessíveis
+// a outras classes fora do pacote declarados.
+
+//Default
+//A classe e/ou seus membros são acessíveis somente por classes 
+// do mesmo pacote, na sua declaração não é definido nenhum tipo de modificador
+// sendo este identificado pelo compilador.
+
