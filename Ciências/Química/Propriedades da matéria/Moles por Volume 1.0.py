e9a341476719aa68e65b36415be17dac0b2daf04
Kioolz <caio.vorb@yahoo.com.br>
2022-12-11 22:29:03 -0300

    Create Moles por Volume 1.0.py

---
+++ Ciências/Química/Propriedades da matéria/Moles por Volume 1.0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 14:19:16 2019
+
+@author: Caio
+"""
+
+class NumerodeMolesporvolume:
+    
+    def MolsporVolume():
+        N=int(input())
+        print(N)
+        v=int(input())
+        print(v)
+        L=N/v
+        print(L)
+    MolsporVolume()
