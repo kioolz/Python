1977dd4e0e9988b7c61b85186f08038048669b01
Kioolz <caio.vorb@yahoo.com.br>
2022-12-11 21:09:52 -0300

    Create Sequências Numéricas.py

---
+++ Códigos fichados e comentados/Matemática/Sequências Numéricas.py
@@ -0,0 +1,26 @@
+
+# Sequência dos termos numéricos de uma função arbitrária.
+
+# Printa a sequência dos termos da função X^2 até um termo escolhido.
+n = int(input())
+
+
+for i in range(0,n):
+    print(i*i)
+    i = i+1
+    
+    
+    
+ # Printa a sequência dos termos da função X^3 até um termo escolhido
+y = int(input())
+
+for i in range (0,y):
+    print (i*i*i)
+    i = i+1
+    
+    
+    
+#Este código pode se repetir de forma genérica para todos os expoentes possíveis da função print
+    # Como escolher a quantidade de vezes que a função Print deveria exponenciar o argumento X^k ? 
+
+# Sendo X a base e K o expoente de valor inteiro.
