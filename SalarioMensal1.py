105b76c0c4e2669e34462ad2367f1cfcfde16403
Kioolz <caio.vorb@yahoo.com.br>
2023-01-04 07:27:40 -0300

    Update SalarioMensal1.py

--- SalarioMensal1.py
+++ SalarioMensal1.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jun 10 17:10:23 2019
-
 @author: Caio
 """
-
 #-----------------------------------------------------------------------------
 # Teste, exibir o Salário de um trabalhador em uma empresa
 
 def main():
-
     Dias = int(input('Numero de Dias Trabalhados :'))
     Horas = int(input('Numero de Horas trabalhadas:'))
     ValorPorHora = int(input('Digite o quanto você ganha por hora:'))
@@ -17,14 +14,11 @@ def main():
     print ("O empregado trabalhou",Dias,"dias", "Totalizando", Horas,"Horas mensais", "e recebeu",
            ValorPorHora,"por cada hora de trabalho")
     print("Seu salario neste mês foi", SalarioMensal)
-    
-    
-#------------------------------------------------------------------------------
+#----------------------------------------------------------------------------
 # Agora vamos comparar o salário dos trabalhadores 
-    
     if (SalarioMensal > 5000) and (SalarioMensal <= 10000):
         print ('Este funcionario ganha entre 5 a 10 salarios minimos')
-    else:
+    elif:
         print('Este funcionario ganha abaixo de 5 salario minimos ou acima de 10 salarios minimos')
 #-----------------------------------------------------------------------------
 
@@ -32,18 +26,16 @@ def main():
 # Atribuir a cada trabalhador seu respectivo salário
 #Comparar o salário dos trabalhadores
 #Tentar agrupar a quantidade de trabalhadores de acordo com suas faixas salariais
-        
-    
 main()
 
-def main():
+def informacoes():
     
     nome = input('Qual o seu nome?')
     idade = int(input('Quantos anos você tem?'))
     peso = float(input ('Quantos quilos você tem?'))
     print ("Seu nome e", nome,"Sua idade e ", idade,"Seu peso e", peso)
     
-main()
+informacoes()
 
 
 
