d983b6ef7caf9f8ad9794df9f0d3bc8449c4f5f2
Kioolz <caio.vorb@yahoo.com.br>
2023-01-04 08:22:02 -0300

    Update Ex - Matriz.py

--- Ciências/Matemática/Ex - Matriz.py
+++ Ciências/Matemática/Ex - Matriz.py
@@ -31,6 +31,11 @@ def make_matrix(num_rows,num_cols,entry_fn):
     return[[entry_fn(i,j) 
     for j in range(num_cols)
     for i in range(num_rows)]
+    
+#Definindo a matriz Diagonal 5 x 5
+    
+def is_diagonal(i,j):
+    return 1 if i==j else 0
 
 
 
