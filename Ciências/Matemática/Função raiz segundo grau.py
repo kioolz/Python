6b34eccddd5fbc23a612c277f90d8e473d3d572b
Kioolz <caio.vorb@yahoo.com.br>
2023-01-04 08:34:35 -0300

    Update Função raiz segundo grau.py

--- Ciências/Matemática/Função raiz segundo grau.py
+++ Ciências/Matemática/Função raiz segundo grau.py
@@ -12,32 +12,9 @@ Created on Mon Jun 17 11:37:26 2019
 # Próximo passo é colocar um botão para o usuário definir cada parâmetro da variável
 # Colocar um comando que printe o gráfico da função de segundo grau.
 
-#Os três métodos funcionam - Preciso de uma estrutura condicional para que o Delta funcione. 
-
-
-
 from math import sqrt
 
-a=float(input('Insira o valor do coeficiente A'))
-b=float(input('Insira o valor do coeficiente B'))
-c=float(input('Insira o valor do coeficiente C'))
-
-y1=(-b+(b*b-4*a*c)**0.5)/(2*a)
-y2=(-b-(b*b-4*a*c)**0.5)/(2*a)
-
-y1a=2*c/(-b-sqrt(b*b-4*a*c))
-y2a=2*c/(-b+sqrt(b*b-4*a*c))
-print('Raiz 1 =', y1, 'Raiz 1 alternativa=' , y1a)
-print('Raiz 2 =', y2, 'Raiz 2 alternativa=' , y2a)
-
-
-#--------------------------------------------------------------------------------------#
-
-from math import sqrt
-
-a=float(input('Insira o valor do coeficiente A'))
-b=float(input('Insira o valor do coeficiente B'))
-c=float(input('Insira o valor do coeficiente C'))
+a=-1 ; b=88550000; c=-1;
 
 y1=(-b+sqrt(b*b-4*a*c))/(2*a)
 y2=(-b-sqrt(b*b-4*a*c))/(2*a)
@@ -47,18 +24,5 @@ y2a=2*c/(-b+sqrt(b*b-4*a*c))
 print('Raiz 1 =', y1, 'Raiz 1 alternativa=' , y1a)
 print('Raiz 2 =', y2, 'Raiz 2 alternativa=' , y2a)
 
-#---------------------------------------------------------------------------------------#
 
-from math import sqrt
-
-a=float(input('Insira o valor do coeficiente A'))
-b=float(input('Insira o valor do coeficiente B'))
-c=float(input('Insira o valor do coeficiente C'))
-
-y1 = ((b*(-1)) + sqrt(b*b-4*a*c)) / (2 * a)
-y2 = ((b*(-1)) - sqrt(b*b-4*a*c)) / (2 * a)
-
-y1a=2*c/(-b-sqrt(b*b-4*a*c))
-y2a=2*c/(-b+sqrt(b*b-4*a*c))
-print('Raiz 1 =', y1, 'Raiz 1 alternativa=' , y1a)
-print('Raiz 2 =', y2, 'Raiz 2 alternativa=' , y2a)
+#--------------------------------------------------------------------------------------#
