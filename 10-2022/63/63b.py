5039d3d07afc80eca50de8a478da3565f1295510
Kioolz <caio.vorb@yahoo.com.br>
2023-01-05 21:22:25 -0300

    Delete 63b.py

--- 10-2022/63/63b.py
+++
@@ -1,8 +0,0 @@
-#Obs.: Resolva este problema utilizando busca em vetor com intervalos ao invés de
-#encadear estruturas de seleção.
-#Leia um número inteiro que representa um código de DDD para discagem
-#interurbana. Em seguida, informe à qual cidade o DDD pertence, considerando a tabela
-#abaixo
-
-n = int(input())
-
