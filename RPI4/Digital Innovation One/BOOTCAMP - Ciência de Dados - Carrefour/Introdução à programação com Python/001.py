ad20062570e391b6b770b10b1a3e66cc6fc15b8f
Kioolz <caio.vorb@yahoo.com.br>
2023-01-06 08:09:12 -0300

    Create 001.py

---
+++ RPI4/Digital Innovation One/BOOTCAMP - Ciência de Dados - Carrefour/Introdução à programação com Python/001.py
@@ -0,0 +1,20 @@
+a = input('Entre com o primeiro valor: ')
+b = input('Entre com o segundo valor: ')
+
+soma = a+b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+
+print ('Soma: {soma}. \nSubtração: {subtracao}.'
+
+resultado='\nMultiplicação: {multiplicacao}'
+'\nDivisão: {divisao}'
+'\nResto: {resto}'.format(soma=soma,
+subtracao=subtracao,
+resto=resto,
+multiplicacao=multiplicacao,
+divisao=divisao))
+
+print(resultado)
