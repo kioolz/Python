f5449a0d7a779f68617968ba0711d9645a05e2ec
Kioolz <caio.vorb@yahoo.com.br>
2023-06-22 09:17:08 -0300

    Create Fiesta.java

---
+++ Java/BOOTCAMP - Santander - FullStack/Desenvolvimento básico em Java/004 - interfaces/Fiesta.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation.interfaces;
+
+public class Fiesta implements Carro, Veiculo {
+    
+    @Override
+    public string marca() { return "Ford"; }
+
+    @Override
+    public String registro() { return "123AFG547TR"; }
+
+    @Override
+    public void ligar () { 
+        Carro.super.ligar();
+
+        Veiculo.super.ligar();
+
+
+    }
+}
