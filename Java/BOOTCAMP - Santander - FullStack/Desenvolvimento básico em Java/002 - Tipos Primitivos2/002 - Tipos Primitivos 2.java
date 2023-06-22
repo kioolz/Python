b3eded5f84c67bb97d956d22756eef1e6d3ba6c3
Kioolz <caio.vorb@yahoo.com.br>
2023-06-22 09:17:06 -0300

    Create 002 - Tipos Primitivos 2.java

---
+++ Java/BOOTCAMP - Santander - FullStack/Desenvolvimento básico em Java/002 - Tipos Primitivos2/002 - Tipos Primitivos 2.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation.tipos.primitovs;
+
+
+public class DefaultValues {
+    
+    public static void main(String[] args) {
+
+        final Default d = new Default();
+
+        System.out.println(d.getI());
+        System.out.println(d.isActive());
+
+    }
+}
+
+class Default {
+    int i;
+    boolean active;
+    public int getI() { return i; }
+    public boolean isActive() { return active; }
+    
+}
+
+
