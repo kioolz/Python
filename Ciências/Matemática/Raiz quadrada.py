44ec217682fde01c3c8c9318416b0b98861d4db9
Kioolz <caio.vorb@yahoo.com.br>
2023-08-18 09:13:42 -0300

    Correções
    Correções e apagando códigos velhos que podem ser agregados em outro arquivo...

--- Ciências/Matemática/Raiz quadrada.py
+++ Ciências/Matemática/Raiz quadrada.py
@@ -5,13 +5,20 @@ Created on Mon Jul 29 23:40:35 2019
 @author: Caio
 """
 
+from numpy.lib.scimath import sqrt
+
+def raizQuadrada():
+    a = float(input("Insira o valor da constante A"))
+    b = float(input("Insira o valor da constante B"))
+    c = float(input("Insira o valor d"))
+
 a = 1  
 b = 2
 c = 100 
 
 # Coeficientes polinômiais
 
-from numpy.lib.scimath import sqrt
+
 
 r1 = (-b+sqrt(b**2-4*a*c))/(2*a)
 r2 = (-b-sqrt(b**2-4*a*c))/(2*a)
